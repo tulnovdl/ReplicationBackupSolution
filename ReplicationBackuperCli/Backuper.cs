51af0a44307c6c5236fd3b16396f1213f743d60a
tulnov_dl <tulnov_dl@TULNOV_DL>
2018-07-09 15:29:22 +0300

    init

---
+++ ReplicationBackuperCli/Backuper.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.SqlServer.Management.Smo.Agent;
+using Microsoft.SqlServer.Replication;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReplicationBackuperCli
+{
+    class Backuper
+    {
+        public static void test()
+        {
+            string FolderDate;
+            string NetworkPath;
+
+            String instance = @"server\instance";
+            String userName = @"username";
+            String password = @"password";
+
+            Server srv = ServerLinkFactory.GetServerLink(instance, userName, password);
+            ReplicationServer RS = ServerLinkFactory.GetReplicationServerLink(instance, userName, password);
+
+            FolderDate = DateTime.Now.ToShortDateString().Replace('\\', '_').Replace('/', '_');
+            NetworkPath = @"\\confluence\D$" + @"\TestBackups" + @"\ReplicaitonBackups\" + FolderDate + "\\";
+            Directory.CreateDirectory(NetworkPath);
+
+            try
+            {
+                foreach (ReplicationDatabase RD in RS.ReplicationDatabases)
+                {
+                    if (RD.HasPublications)
+                    {
+                        foreach (TransPublication TP in RD.TransPublications)
+                        {
+                            TextWriter tw = new StreamWriter(NetworkPath + "\\" + TP.Name.ToString() + ".sql");
+                            tw.Write(TP.Script(ScriptOptions.Creation | ScriptOptions.IncludeAll ^ ScriptOptions.IncludeReplicationJobs));
+                            tw.Close();
+                        }
+                    }
+                }
+            }
+
+            catch (Exception eh)
+            {
+                //MessageBox.Show(eh.ToString());
+            }
+
+            StringCollection strCol = new StringCollection();
+            ScriptingOptions scriptOpt = new ScriptingOptions();
+            scriptOpt.IncludeDatabaseContext = true;
+
+            try
+            {
+                string script = "";
+                string JobName;
+                //Looping through the job
+                foreach (Job J in srv.JobServer.Jobs)
+                {
+                    script = "";
+                    JobName = J.Name.ToString();
+                    strCol = J.Script(scriptOpt);
+
+                    //concate the text of job
+                    foreach (string s in strCol)
+                    {
+                        script += s;
+                    }
+                    //save the job file
+                    string dir = @"\\confluence\D$" + @"\TestBackups" + @"\JobBackups\" + FolderDate + "\\";
+                    System.IO.Directory.CreateDirectory(dir);
+                    string fullPath = dir + JobName.Replace(':', '_').ToString() + @".sql";
+                    TextWriter tw = new StreamWriter(fullPath);
+                    tw.Write(script);
+                    tw.Close();
+                }
+            }
+            catch
+            {
+            }
+
+        }
+
+    }
+}
+
---
+++ ReplicationBackuperCli/ServerLinkFactory.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.SqlServer.Management.Smo.Agent;
+using Microsoft.SqlServer.Replication;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReplicationBackuperCli
+{
+    class ServerLinkFactory
+    {
+        String instance;
+        String userName;
+        String password;
+        Server server;
+
+        public static Server GetServerLink(String instance, String userName, String password)
+        {
+            ServerConnection conn = new ServerConnection(instance, userName, password);
+            return new Server(conn);
+        }
+
+        public static ReplicationServer GetReplicationServerLink(String instance, String userName, String password)
+        {
+            ServerConnection conn = new ServerConnection(instance, userName, password);
+            return new ReplicationServer(conn);
+        }
+    }
+}
